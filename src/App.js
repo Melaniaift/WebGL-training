232faa85e8ed63a5cdf2c972c7f1a98556be8fde
Melaniaift <76125731+Melaniaift@users.noreply.github.com>
2024-10-08 10:04:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Contact } from './pages/Contact';
